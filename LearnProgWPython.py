8c0131c9ee6a53d50ab89ac371a751f7138bb561
MrPeaBody <James3roberts@gmail.com>
2020-04-13 15:12:48 -0600

    Variable

--- LearnProgWPython.py
+++ LearnProgWPython.py
@@ -12,7 +12,17 @@
 # print ('string',x)
 # #also can  do this
 # print('string'+'also')#have to watch spaces here. 
-#Ctrl+c+k comments out a block of code
+#Ctrl+k+c comments out a block of code
 #Ctrl+k+u uncomments out a block of code
+#Naming variables
+# car_speed = 100
+# room_temp = 30
+# age = 34
+# print('My Age is', age, 'years old.')# commas add spaces in strings automatically
+# score = 89
+# score = 90
+# print(score)
+#if you use the same variable name it will only use the last one written
+
 
 
