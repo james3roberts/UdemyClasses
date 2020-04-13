6c48d39f8f69a4efa40c51e33f0d7bbbe3c10b86
MrPeaBody <James3roberts@gmail.com>
2020-04-13 14:49:12 -0600

    section 2 chap2

--- LearnProgWPython.py
+++ LearnProgWPython.py
@@ -1,3 +1,6 @@
 ##This class claims to take me from zero to hero.
 # I skipped the 1st section to get to the actual programming
-# s2-c1 Data types
+# s2-c1 Data types Print statements. Vapriables 1,2,3, naming, values, comments input str(), operators, real worls 1-6
+print('My name is James')
+
+
