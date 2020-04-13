fae4d8aa4f3b4ac7eaf96778d56f1a306ef46450
MrPeaBody <James3roberts@gmail.com>
2020-04-13 14:57:57 -0600

    print statements

--- LearnProgWPython.py
+++ LearnProgWPython.py
@@ -1,6 +1,18 @@
 ##This class claims to take me from zero to hero.
 # I skipped the 1st section to get to the actual programming
 # s2-c1 Data types Print statements. Vapriables 1,2,3, naming, values, comments input str(), operators, real worls 1-6
-print('My name is James')
+# print('This is how you print a string')
+# #this is how you set a variable
+# x = 1
+# #this is how you print a variable
+# print(x)
+# #this is wrong
+# print('x')
+# #this is how you combine them dont know it its part of the learning proccess
+# print ('string',x)
+# #also can  do this
+# print('string'+'also')#have to watch spaces here. 
+#Ctrl+c+k comments out a block of code
+#Ctrl+k+u uncomments out a block of code
 
 
